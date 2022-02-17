f839c38affdc12271441fe5b2a1be240d5a88639
Fabrizio <fabriziogomespereira@hotmail.com>
2022-02-17 16:30:58 -0300

    Exibindo post e categoria

--- petshop/src/componentes/ListaPost.jsx
+++ petshop/src/componentes/ListaPost.jsx
@@ -1,35 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom'
-import { busca } from '../api/api'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { busca } from "../api/api";
 
-const ListaPost = ({url}) => {
-    
-    const [posts, setPosts] = useState([])
+const ListaPost = ( { url } ) => {
+  const [posts, setPosts] = useState([]);
 
-    useEffect(()=> {
-        busca(url, setPosts)
-    }, [])
+  useEffect(() => {
+    busca(url, setPosts);
+  }, [url])
 
-    return(
-        <section className='posts container'>
-            {
-                posts.map((post)=>(
-                    <Link className={`cartao-post cartao-post--${post.categoria}`}>
-                        <article key={post.id}>
-                            <h3 className='cartao-post__titulo'>
-                                {post.title}
-                            </h3>
-                            <p className='cartao-post__meta'>
-                                {post.metadescription}
-                            </p>
-                        </article>
-                        </Link>
-                ))
-            }
+  return (
+    <section className="posts container">
+      {posts.map((post) => (
+        <Link className={`cartao-post cartao-post--${post.categoria}`} to={`/posts/${post.id}`}>
+          <article key={post.id}>
+            <h3 className="cartao-post__titulo">{post.title}</h3>
+            <p className="cartao-post__meta">{post.metadescription}</p>
+          </article>
+        </Link>
+      ))}
+    </section>
+  );
+};
 
-        </section>
-
-    )
-}
-
-export default ListaPost
+export default ListaPost;
